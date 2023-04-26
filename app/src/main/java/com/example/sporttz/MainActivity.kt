eff13aebf4225ecbecdcba3adf4222e1b4560a5b
mrocznaczarownica <mr0cznaczar0wn1caa@gmail.com>
2023-04-27 03:44:53 +0400

    уже почти

--- app/src/main/java/com/example/sporttz/MainActivity.kt
+++ app/src/main/java/com/example/sporttz/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.sporttz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val homeIntent = Intent(this@MainActivity, WebViewActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Do some stuff here, like implement deep linking
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+    companion object {
+        const val SPLASH_TIME_OUT = 3000
     }
 }
---
+++ app/src/main/java/com/example/sporttz/WebViewActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sporttz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+    }
+}
